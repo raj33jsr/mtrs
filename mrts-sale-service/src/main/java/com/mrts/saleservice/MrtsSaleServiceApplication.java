6a8236fbcfd040ff4fb6ab7bf11075cf738d8272
Milind <milind.db88@gmail.com>
2019-09-20 14:36:19 +0530

    initial commit

---
+++ mrts-sale-service/src/main/java/com/mrts/saleservice/MrtsSaleServiceApplication.java
@@ -0,0 +1,13 @@
+package com.mrts.saleservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication(scanBasePackages = "{com.mrts.saleservice}")
+public class MrtsSaleServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MrtsSaleServiceApplication.class, args);
+	}
+
+}
---
+++ mrts-sale-service/src/main/java/com/mrts/saleservice/domain/Sale.java
@@ -0,0 +1,114 @@
+/**
+ * 
+ */
+package com.mrts.saleservice.domain;
+
+import java.io.Serializable;
+import java.util.Date;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.Field;
+
+/**
+ * @author bharamil
+ *
+ */
+@Document(collection = "mrts_sale")
+public class Sale implements Serializable {
+
+    /**
+     * 
+     */
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    private String id;
+
+    @Field(value = "personId")
+    private String personId;
+
+    @Field(value = "ticketId")
+    private String ticketId;
+
+    @Field(value = "startDate")
+    private Date startDate;
+
+    @Field(value = "enddate")
+    private Date enddate;
+
+    /**
+     * @return the id
+     */
+    public String getId() {
+        return id;
+    }
+
+    /**
+     * @param id
+     *            the id to set
+     */
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    /**
+     * @return the personId
+     */
+    public String getPersonId() {
+        return personId;
+    }
+
+    /**
+     * @param personId
+     *            the personId to set
+     */
+    public void setPersonId(String personId) {
+        this.personId = personId;
+    }
+
+    /**
+     * @return the ticketId
+     */
+    public String getTicketId() {
+        return ticketId;
+    }
+
+    /**
+     * @param ticketId
+     *            the ticketId to set
+     */
+    public void setTicketId(String ticketId) {
+        this.ticketId = ticketId;
+    }
+
+    /**
+     * @return the startDate
+     */
+    public Date getStartDate() {
+        return startDate;
+    }
+
+    /**
+     * @param startDate
+     *            the startDate to set
+     */
+    public void setStartDate(Date startDate) {
+        this.startDate = startDate;
+    }
+
+    /**
+     * @return the enddate
+     */
+    public Date getEnddate() {
+        return enddate;
+    }
+
+    /**
+     * @param enddate
+     *            the enddate to set
+     */
+    public void setEnddate(Date enddate) {
+        this.enddate = enddate;
+    }
+
+}
